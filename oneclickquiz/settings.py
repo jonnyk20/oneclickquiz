41f4e4652f8261dedaeadcfd798975202583cf79
Jonny Kalambay <jonnyk20@gmail.com>
2020-02-02 19:24:54 -0800

    Moving react files to django project

--- oneclickquiz/settings.py
+++ oneclickquiz/settings.py
@@ -15,10 +15,6 @@ import os
 import django_heroku
 
 
-# Configure app for Heroku deployment
-django_heroku.settings(locals())
-
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -127,3 +123,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# # Configure app for Heroku deployment
+django_heroku.settings(locals())
---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import "./App.scss";
+
+const convertArrayToString = arr => arr.join("\n");
+
+const createQuiz = async items => {
+  const formData = new FormData();
+
+  formData.append("items", items);
+
+  const response = await fetch("/create/", {
+    method: "POST",
+    body: formData
+  });
+  const json = await response.json();
+
+  return json;
+};
+
+const App = () => {
+  const [items, setItems] = useState(
+    convertArrayToString(["Tiger", "Leopard", "Cheetah"])
+  );
+  const [inputValue, setInputValue] = useState(items);
+
+  const handleChange = event => {
+    setInputValue(event.target.value);
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    createQuiz(inputValue);
+  };
+
+  return (
+    <div className="app">
+      <h1>1 Click Quiz</h1>
+      <form onSubmit={handleSubmit}>
+        <textarea value={inputValue} onChange={handleChange} />
+        <br />
+        <button type="submit">Create Quiz</button>
+      </form>
+    </div>
+  );
+};
+
+export default App;
