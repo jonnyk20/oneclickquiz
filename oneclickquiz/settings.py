5256d7e3d57d9a40e5403359e01f42748d69f20e
Jonny Kalambay <jonnyk20@gmail.com>
2020-02-02 20:01:26 -0800

    React app working

--- oneclickquiz/settings.py
+++ oneclickquiz/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'oneclickquiz.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'build'), ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,8 +121,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'build'),
+    os.path.join(BASE_DIR, 'build', 'static'),
+)
 
 # # Configure app for Heroku deployment
 django_heroku.settings(locals())
