54c81a95be269aa7d407e52f75ea5a903de72a3d
Jonny Kalambay <jonnyk20@gmail.com>
2020-02-06 21:01:31 -0800

    Formatting quiz

--- quiz_builder/quiz_builder.py
+++ quiz_builder/quiz_builder.py
@@ -1,8 +1,26 @@
 from .models import QuizType, Quiz, Question, Choice, ItemType, Item
 
 
-def buildQuiz():
-    output = {}
+
+def formatChoice(choice):
+    item = choice.item
+    formattedChoice = item.data
+    return formattedChoice
+
+def formatQuestion(question):
+    formattedQuestion = {
+        'correct_answer': question.correct_answer,
+        'choices': [formatChoice(c) for c in question.choice_set.all()]
+    }
+    return formattedQuestion
+
+
+def formatQuiz():
     quiz = Quiz.objects.first()
-    print('QUIZ', quiz)
-    return quiz
+    formattedQuiz = {
+        'id': quiz.id,
+        'quiz_type': quiz.quiz_type.description,
+        'questions': [formatQuestion(q) for q in quiz.question_set.all()],
+        'max_score': quiz.question_set.count()
+    }
+    return formattedQuiz
--- quiz_builder/views.py
+++ quiz_builder/views.py
@@ -2,8 +2,7 @@
 from django.http import JsonResponse
 from .forms import ItemsForm
 from django.views.decorators.csrf import csrf_exempt
-from .quiz_builder import buildQuiz
-
+from .quiz_builder import formatQuiz
 
 def parse_string(string):
     items = string.split('\n')
@@ -12,15 +11,13 @@ def parse_string(string):
 
 @csrf_exempt
 def createQuiz(request):
-    print('>>>>>>>>>>>>>')
     if request.method == 'POST':
         form = ItemsForm(request.POST)
         if form.is_valid():
             items = request.POST['items']
             items = parse_string(items)
-            quiz = buildQuiz()
+            quiz = formatQuiz()
 
-            return JsonResponse({'items': items}, safe=False)
+            return JsonResponse({'quiz': quiz}, safe=False)
         else:
-            print('FAIL')
             return JsonResponse({})
